60ec307cba7c04f2c3553717915e5f5ef3d7c9d8
AkatsukiSec <58699578+AkatsukiSec@users.noreply.github.com>
2020-01-30 11:24:42 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
-apt-get update
-apt-get upgrade
-apt-get install git build-essential autoconf libnl-genl-3-dev pkg-config libcairo2-dev feh libxcb-composite0-dev xcb-proto libxcb-ewmh-dev python-xcbgen terminator cmake compton libxcb-shape0-dev libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev xcb libxcb1-dev libxcb-icccm4-dev libyajl-dev libev-dev libxcb-xkb-dev libxcb-cursor-dev libxkbcommon-dev libxcb-xinerama0-dev libxkbcommon-x11-dev libstartup-notification0-dev libxcb-randr0-dev libxcb-xrm0 libxcb-xrm-dev
-apt-get install i3 # to get dependencies
+apt-get update 
+apt-get upgrade -y
+apt-get install -y git build-essential autoconf libnl-genl-3-dev pkg-config libcairo2-dev feh libxcb-composite0-dev xcb-proto libxcb-ewmh-dev python-xcbgen terminator cmake compton libxcb-shape0-dev libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev xcb libxcb1-dev libxcb-icccm4-dev libyajl-dev libev-dev libxcb-xkb-dev libxcb-cursor-dev libxkbcommon-dev libxcb-xinerama0-dev libxkbcommon-x11-dev libstartup-notification0-dev libxcb-randr0-dev libxcb-xrm0 libxcb-xrm-dev
+apt-get install -y i3 # to get dependencies
 mkdir gui && cd gui
 git clone https://www.github.com/Airblader/i3 i3-gaps
 cd i3-gaps
