fb42eb9de9499633654f2e833339010d5125d308
AkatsukiSec <58699578+AkatsukiSec@users.noreply.github.com>
2020-01-30 10:12:55 +0100

    added dependency for polybar network module

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 apt-get update
 apt-get upgrade
-apt-get install git build-essential autoconf pkg-config libcairo2-dev feh libxcb-composite0-dev xcb-proto libxcb-ewmh-dev python-xcbgen terminator cmake compton libxcb-shape0-dev libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev xcb libxcb1-dev libxcb-icccm4-dev libyajl-dev libev-dev libxcb-xkb-dev libxcb-cursor-dev libxkbcommon-dev libxcb-xinerama0-dev libxkbcommon-x11-dev libstartup-notification0-dev libxcb-randr0-dev libxcb-xrm0 libxcb-xrm-dev
+apt-get install git build-essential autoconf libnl-genl-3-dev pkg-config libcairo2-dev feh libxcb-composite0-dev xcb-proto libxcb-ewmh-dev python-xcbgen terminator cmake compton libxcb-shape0-dev libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev xcb libxcb1-dev libxcb-icccm4-dev libyajl-dev libev-dev libxcb-xkb-dev libxcb-cursor-dev libxkbcommon-dev libxcb-xinerama0-dev libxkbcommon-x11-dev libstartup-notification0-dev libxcb-randr0-dev libxcb-xrm0 libxcb-xrm-dev
 apt-get install i3 # to get dependencies
 mkdir gui && cd gui
 git clone https://www.github.com/Airblader/i3 i3-gaps
@@ -15,8 +15,8 @@ mkdir -p build && cd build/
 
 cd ../../
 
-wget https://github.com/polybar/polybar/releases/download/3.4.1/polybar-3.4.1.tar
-tar xvf polybar-3.4.1.tar
+wget https://github.com/polybar/polybar/releases/download/3.4.2/polybar-3.4.2.tar
+tar xvf polybar-3.4.2.tar
 
 cd polybar
 mkdir build
